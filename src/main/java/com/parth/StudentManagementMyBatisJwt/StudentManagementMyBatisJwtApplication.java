b197cabc63f433221d178f343fd95ae2691cd265
I3-Parth <parth.shah@techprescient.com>
2024-03-26 16:15:37 +0530

    Initial Commit

---
+++ src/main/java/com/parth/StudentManagementMyBatisJwt/StudentManagementMyBatisJwtApplication.java
@@ -0,0 +1,13 @@
+package com.parth.StudentManagementMyBatisJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementMyBatisJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementMyBatisJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parth/StudentManagementMyBatisJwt/StudentManagementMyBatisJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parth.StudentManagementMyBatisJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementMyBatisJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
