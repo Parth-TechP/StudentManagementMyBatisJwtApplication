1f0ebdd45132feea6da9b947b3cad78e99c2a4b3
I3-Parth <parth.shah@techprescient.com>
2024-05-15 10:39:58 +0530

    f4: multiple objects accepted and printed using kafka

--- src/main/java/com/parth/StudentManagementMyBatisJwt/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/parth/StudentManagementMyBatisJwt/config/kafka/KafkaConsumerConfig.java
@@ -6,8 +6,10 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -23,7 +25,7 @@ public class KafkaConsumerConfig {
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.parth.StudentManagementMyBatisJwt.dto");
+    props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
     return props;
   }
 
@@ -33,8 +35,8 @@ public class KafkaConsumerConfig {
   }
 
   @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+  public ConcurrentKafkaListenerContainerFactory<String,Object> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
     factory.setConsumerFactory(consumerFactory());
     return factory;
   }
--- src/main/java/com/parth/StudentManagementMyBatisJwt/services/KafkaStudentService.java
+++ src/main/java/com/parth/StudentManagementMyBatisJwt/services/KafkaStudentService.java
@@ -1,13 +1,10 @@
 package com.parth.StudentManagementMyBatisJwt.services;
 
 import com.parth.StudentManagementMyBatisJwt.dto.StudentAdditionDto;
-import com.parth.StudentManagementMyBatisJwt.dto.StudentDisplayDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class KafkaStudentService {
@@ -16,9 +13,26 @@ public class KafkaStudentService {
   StudentService studentService;
 
   @KafkaListener(topics = "student-info", groupId = "group1")
-  public void addStudents(List<StudentAdditionDto> studentAdditionDtos) {
+  public void addStudents(StudentAdditionDto studentAdditionDtos){
+//    List<StudentDisplayDto> studentDisplayDtos = new ArrayList<>();
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    for (StudentAdditionDto studentAdditionDto : studentAdditionDtos) {
+//      StudentAdditionDto studentAdditionDto1 = objectMapper.readValue(studentAdditionDto.toString(), StudentAdditionDto.class);
+//      studentDisplayDtos.add(studentService.addStudent(studentAdditionDto1));
+//    }
+
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    List<Map<String, StudentAdditionDto>> studentAdditionDtos = (List<Map<String, StudentAdditionDto>>) objectMapper.readValue(payload, StudentAdditionDto.class);
+//
+//    for(Map<String, StudentAdditionDto> studentAdditionDtoMap: studentAdditionDtos){
+//      StudentAdditionDto studentAdditionDto = new StudentAdditionDto();
+//      studentAdditionDto.setName((String) studentAdditionDtoMap.get("name"));
+//    }
+
+//    System.out.println("students data: "+ payload);
 //    List<StudentDisplayDto> studentDisplayDtos = new ArrayList<>();
 //    for (StudentAdditionDto studentAdditionDto : studentAdditionDtos) {
+//      System.out.println("current Student: "+ studentAdditionDto);
 //      studentDisplayDtos.add(studentService.addStudent(studentAdditionDto));
 //    }
     System.out.println(studentAdditionDtos);
